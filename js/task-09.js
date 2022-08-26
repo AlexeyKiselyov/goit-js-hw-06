b63a27630ebb25af11b7d6e2934c84c35265f2b1
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-08-26 19:34:36 +0300

    Add hw-js #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const buttonChangeColor = document.querySelector('.change-color');
+
+const spanColor = document.querySelector('.color');
+
+const body = document.querySelector('body');
+
+buttonChangeColor.addEventListener('click', onColorChange)
+
+function onColorChange(e) {
+  const randomColor = getRandomHexColor(); 
+  
+  body.style.backgroundColor = randomColor;
+
+  spanColor.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

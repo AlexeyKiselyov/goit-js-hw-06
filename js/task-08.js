bf0324f36c1a861c603e4b0c6d2a7c92c9e41cfe
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-08-26 19:10:16 +0300

    Add hw-js #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  
+  const formElement = e.currentTarget.elements;    
+  const mail = formElement.email.value;
+  const password = formElement.password.value;
+  
+  if (!formElement.email.value || !formElement.password.value) {
+    alert("All inputs must be completed")
+    return
+    }
+
+  const formData = {
+    mail,
+    password
+  }
+
+  console.log(formData);
+  form.reset();
+}

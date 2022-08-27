4d6f81452667e3ac7e46964f4269fb098d329e3c
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-08-27 22:18:10 +0300

    Add hw-js #10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const createBtn = document.querySelector('[data-create]');
+
+const destroyBtn = document.querySelector('[data-destroy]');
+
+const box = document.querySelector('#boxes');
+
+const input = document.querySelector('input');
+
+createBtn.addEventListener('click', onCreateBoxes);
+
+destroyBtn.addEventListener('click', onDestroy);
+
+function onCreateBoxes() {
+  let amount = Number(input.value);
+  createBoxes(amount);
+}
+
+function onDestroy() {
+  box.innerHTML = '';
+  input.value = '';
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  let arr = [];
+  for (let i = 1; i <= amount; i += 1) {
+    const randomColor = getRandomHexColor();
+    let string = `<div style= "width: ${size}px; height: ${size}px; background-color: ${randomColor};" ></div> `;
+    size += 10;
+    arr.push(string);
+  }
+  const stringFinal = arr.join('');
+  box.insertAdjacentHTML('beforeend', stringFinal);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
